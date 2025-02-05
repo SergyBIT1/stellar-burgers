80446b0df232b6d63b485233524170a6ce9202e9
SergyBIT1 <slavnij@gmail.com>
2025-02-05 17:17:59 +0300

    Доделал app и protected-route

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -6,10 +6,11 @@ import { BurgerIngredients } from '../../components';
 import { BurgerConstructor } from '../../components';
 import { Preloader } from '../../components/ui';
 import { FC } from 'react';
+import { getIngredientState } from '../../services/slices/ingredientSlice/ingredientSlice';
 
 export const ConstructorPage: FC = () => {
-  /** TODO: взять переменную из стора */
-  const isIngredientsLoading = false;
+  /** TODO: взял переменную из стора */
+  const isIngredientsLoading = useSelector(getIngredientState).loading;
 
   return (
     <>
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,24 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+// import { TOrder } from '@utils-types';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import {
+  getFeedState,
+  getFeeds
+} from '../../services/slices/feedSlice/feedSlice';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  /** TODO: взял переменную из стора */
+  const { orders, loading } = useSelector(getFeedState);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getFeeds());
+  }, []);
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  <FeedUI orders={orders} handleGetFeeds={() => dispatch(getFeeds())} />;
 };
