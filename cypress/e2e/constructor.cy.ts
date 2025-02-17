1cc43da8a7a73ba6b7f5350d7b12e8325828fae5
SergyBIT1 <slavnij@gmail.com>
2025-02-17 12:50:30 +0300

    Проблемы с CORS

---
+++ cypress/e2e/constructor.cy.ts
@@ -0,0 +1,66 @@
+import Cypress from "cypress";
+
+const baseUrl = 'http://localhost:4000';
+const getUserUrl = 'api/auth/user';
+const orderUrl = 'api/orders'; 
+
+const addButtonBun =
+  '[data-cy=ingredient-item-643d69a5c3f7b9001cfa093d] button[type=button]';
+const addButtonIngredient =
+  '[data-cy=ingredient-item-643d69a5c3f7b9001cfa0943] button[type=button]';
+
+describe ('Создание заказа'  , () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredient.json'
+    }).as('getIngredients')
+    cy.intercept('GET', getUserUrl, {fixture: 'user.json'
+  }).as('getUser')
+    cy.intercept('POST', orderUrl, {fixture: 'order.json'
+  }).as('postOrder')
+
+  window.localStorage.setItem('refreshToken', 'mockRefreshToken')
+  cy.setCookie('accessToken', 'mockAccessToken')
+
+  cy.visit(baseUrl)
+  })
+
+  afterEach(() => {
+    cy.clearCookie('refreshToken')
+    cy.clearCookie('accessToken')
+  })
+
+ describe('Конструктор и создание заказа', () => {
+  it('добавление булочки и ингредиента в конструктор', () => {
+    cy.wait('@getIngredients')
+
+    cy.get('[data-cy=bun-top]').should('not.exist');
+    cy.get('[data-cy=bun-bottom]').should('not.exist');
+
+    // Добавление булки
+    cy.get(addButtonBun).click();
+    // Проверка верхней булки
+    cy.get('[data-cy=bun-top]')
+      .contains('Флюоресцентная булка R2-D3')
+      .should('exist');
+    // Проверка нижней булки
+    cy.get('[data-cy=bun-bottom]')
+      .contains('Флюоресцентная булка R2-D3')
+      .should('exist');
+
+    // Проверяем, что основной ингредиент отсутствует в конструкторе
+    cy.get('[data-cy=between-buns]').should('not.exist'); // Ожидаем, что соус отсутствует
+
+    // Добавление основного ингредиента
+    cy.get(addButtonIngredient).click();
+    // Проверка добавленного ингредиента
+    cy.get('[data-cy=between-buns]')
+      .contains('Соус фирменный Space Sauce')
+      .should('exist');
+  })
+ })
+    
+
+  })
+
+
